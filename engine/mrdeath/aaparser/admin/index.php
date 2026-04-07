bd400ba62ba30e3788d09eacf84a0a13b55ba841
Timmy-Tim-Dev <oswillyo@gmail.com>
2026-04-07 07:21:12 +0500

    07.04.2026 (4.7.3)

--- engine/mrdeath/aaparser/admin/index.php
+++ engine/mrdeath/aaparser/admin/index.php
@@ -357,7 +357,8 @@ elseif ( $action == 'dbupgrade' ) {
 			'4.6.1',
 			'4.6.2',
 			'4.7.0',
-			'4.7.1'
+			'4.7.1',
+			'4.7.3'
 		];
 		
 		$versions = array_filter($versions, function ($verik) use ($log_module_version) {
